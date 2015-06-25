06a9aa11537d92feca05fb92ec79306d84f03fc1
liudong9183 <liudong9183@163.com>
2015-06-25 22:43:36 +0800

    first commit

---
+++ src/com/example/testlayout/BookActivity.java
@@ -0,0 +1,89 @@
+package com.example.testlayout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.*;
+
+public class BookActivity extends Activity {
+	public DbHelper dbhelper = null;
+	public BookItem item = null;
+	public boolean flag = false;
+	public boolean desc_flag = true;
+	public TextView bookname_view = null;
+	public TextView bookauthor_view = null;
+	public TextView bookdesc_view = null;
+	public ImageView image_view = null;
+	public Button savebook_btn = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.setContentView(R.layout.activity_book);
+		dbhelper = new DbHelper(this, "book_store.db", null, 1);
+		Intent intent = this.getIntent();
+		Bundle bundle = intent.getExtras();
+		this.item = (BookItem) bundle.getSerializable("book");
+		image_view = (ImageView) this.findViewById(R.id.book_image);
+		bookname_view = (TextView) this.findViewById(R.id.book_name);
+		bookauthor_view = (TextView) this
+				.findViewById(R.id.book_author);
+		bookdesc_view = (TextView) this.findViewById(R.id.book_desc);
+		bookdesc_view.setText(item.getBookDesc());
+		savebook_btn = (Button) this.findViewById(R.id.save_book);
+		flag = dbhelper.isexist(item);
+		if (flag) {
+			savebook_btn.setText("查看");
+		}
+
+		image_view.setImageBitmap(item.getBitmap());
+		bookname_view.setText(item.getBookName());
+		bookauthor_view.setText(item.getBookAuthor());
+
+		savebook_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (!flag) {
+					if (dbhelper.insertBook(item)) {
+						Toast.makeText(BookActivity.this, "插入成功",
+								Toast.LENGTH_SHORT).show();
+					} else {
+						Toast.makeText(BookActivity.this, "插入成功",
+								Toast.LENGTH_SHORT).show();
+					}
+					Intent intent = new Intent(BookActivity.this,
+							MainActivity.class);
+					startActivity(intent);
+				}
+			}
+		});
+
+		bookdesc_view.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (desc_flag) {
+					desc_flag = false;
+					BookActivity.this.bookdesc_view.setEllipsize(null);
+					BookActivity.this.bookdesc_view.setSingleLine(false);
+				} else {
+					desc_flag = true;
+					BookActivity.this.bookdesc_view
+							.setEllipsize(android.text.TextUtils.TruncateAt.END);
+					BookActivity.this.bookdesc_view.setMaxLines(3);
+				}
+			}
+		});
+	}
+}
---
+++ src/com/example/testlayout/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.testlayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.zxing.activity.*;
+import com.example.*;
+
+public class MainActivity extends Activity {
+	public final String TAG = MainActivity.class.getName();
+	public static int select_flag = 0;
+	private List<BookItem> _booklist = null;
+	private DbHelper dbhelper = null;
+	private ListView listview = null;
+	private ImageButton add_book_btn = null;
+	private ImageButton select_book_btn = null;
+	private ImageButton select_friend_btn = null;
+	private ImageButton select_me_btn = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		dbhelper = new DbHelper(this, "book_store.db", null, 1);
+		listview = (ListView) this.findViewById(R.id.book_list);
+		add_book_btn = (ImageButton) this.findViewById(R.id.add_book_item);
+		
+		add_book_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+				MainActivity.this.startActivity(intent);
+			}
+		});
+		
+		select_book_btn = (ImageButton) this.findViewById(R.id.button_book);
+		select_friend_btn = (ImageButton)this.findViewById(R.id.button_friend);
+		select_me_btn = (ImageButton) this.findViewById(R.id.button_msg);
+		select_book_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				MainActivity.select_flag = 0;
+				select_book_btn.setImageResource(R.drawable.book_selected);
+				select_friend_btn.setImageResource(R.drawable.friend);
+				select_me_btn.setImageResource(R.drawable.me);
+			}
+		});
+		select_friend_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				MainActivity.select_flag = 1;
+				select_book_btn.setImageResource(R.drawable.book);
+				select_friend_btn.setImageResource(R.drawable.book_selected);
+				select_me_btn.setImageResource(R.drawable.me);
+			}
+		});
+		
+		select_me_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				MainActivity.select_flag = 2;
+				select_book_btn.setImageResource(R.drawable.book);
+				select_friend_btn.setImageResource(R.drawable.friend);
+				select_me_btn.setImageResource(R.drawable.book_selected);
+			}
+		});
+		
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		switch (MainActivity.select_flag) {
+		case 0:
+			MainActivity.select_flag = 0;
+			select_book_btn.setImageResource(R.drawable.book);
+		case 1:
+			MainActivity.select_flag = 1;
+			
+		case 2:
+		default:
+			break;
+		}
+		
+		this._booklist = dbhelper.readALLBook();
+		BookAdapter adapter = new BookAdapter(this, R.layout.book_item, this._booklist);
+		listview.setAdapter(adapter);
+		listview.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				BookItem item = MainActivity.this._booklist.get(position);
+				Intent intent = new Intent(MainActivity.this, BookActivity.class);
+				Bundle bundle = new Bundle();
+				bundle.putSerializable("book", item);
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+}
