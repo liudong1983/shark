f88d241eb362f329668bbdee2781bad63d425919
liudong9183 <liudong9183@163.com>
2015-07-02 07:54:20 +0800

    update search activity

--- src/com/example/testlayout/SearchActivity.java
+++ src/com/example/testlayout/SearchActivity.java
@@ -1,14 +1,37 @@
 package com.example.testlayout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.*;
 
 public class SearchActivity extends Activity {
+	public RelativeLayout layout1 = null;
+	public TextView txt = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_serach);
+		
+		layout1 = (RelativeLayout) this.findViewById(R.id.search_layout);
+		txt = (TextView) this.findViewById(R.id.search_txt);
+		layout1.setClickable(true);
+		layout1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				SearchActivity.this.layout1.setAlpha(1f);
+				Toast.makeText(SearchActivity.this, "search button click", Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent(SearchActivity.this, SearchResult.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
---
+++ src/com/example/testlayout/SearchResult.java
@@ -0,0 +1,15 @@
+package com.example.testlayout;
+
+import com.example.*;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchResult extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_ret);
+	}
+
+}
